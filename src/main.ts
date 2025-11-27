1cb4949cf974667e819555956b0f5000ca2464a1
juank0225 <juankarloscardenasr2@gmail.com>
2025-11-27 15:50:44 -0500

    backend de login y perfil funcional

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Configurar CORS para React
+  app.enableCors({
+    origin: 'http://localhost:5173', // o el puerto de tu Vite
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
